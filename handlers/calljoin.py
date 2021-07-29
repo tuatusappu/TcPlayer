4215b7c600cb24fb07431569dfbae8653c4cc923
tuatusappu <71092937+tuatusappu@users.noreply.github.com>
2021-07-29 21:15:58 +0530

    Update calljoin.py

--- handlers/calljoin.py
+++ handlers/calljoin.py
@@ -20,25 +20,25 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "TcPlayerBot"
+        user.first_name =  "LovelyMusic"
 
     try:
         await USER.join_chat(invitelink)
         await USER.send_message(message.chat.id,"I J H A Y RQ")
     except UserAlreadyParticipant:
         await message.reply_text(
-            "<b>@TcPlayer A I Y C</b>",
+            "<b>@LOVELYVCBOT A I Y C</b>",
         )
         pass
     except Exception as e:
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n U {user.first_name} C' J Y G D T H J RQ F U! M S U I N B I G."
-            "\n\nO M A @TcPlayer T Y G A T A</b>",
+            "\n\nO M A @LOVELYVCBOT T Y G A T A</b>",
         )
         return
     await message.reply_text(
-            "<b>@TcPlayer U J Y C</b>",
+            "<b>@LOVELYVCBOT U J Y C</b>",
         )
     
 @USER.on_message(filters.group & filters.command(["userbotleave"]))
